f682dc3c6390aabd8cb70b23421f9e3dc95cc924
Omico <omico@qq.com>
2021-07-18 20:26:30 -0700

    Add notice for SigningConfigs

--- src/main/kotlin/me/omico/age/SigningConfigs.kt
+++ src/main/kotlin/me/omico/age/SigningConfigs.kt
@@ -4,15 +4,16 @@ package me.omico.age
 
 import com.android.build.gradle.BaseExtension
 import org.gradle.api.Project
+import java.io.File
 import java.util.Properties
 
-fun Project.configureSigningConfigs(
-    name: String = "release",
+fun Project.configureAppSigningConfigsForRelease(
     properties: Properties = localProperties,
 ) = plugins.withId("com.android.application") {
+    if (hasNullSigningConfigProperties(properties)) return@withId
     configure<BaseExtension>("android") {
         signingConfigs {
-            create(name) {
+            create("release") {
                 storeFile = file(properties["store.file"] as String)
                 storePassword = properties["store.password"] as String
                 keyAlias = properties["key.alias"] as String
@@ -20,4 +21,18 @@ fun Project.configureSigningConfigs(
             }
         }
     }
+}
+
+private fun Project.hasNullSigningConfigProperties(properties: Properties): Boolean {
+    val nullProperties = listOf("store.file", "store.password", "key.alias", "key.password")
+        .mapNotNull { if (properties[it] == null) it else null }
+    val hasNullProperties = nullProperties.isNotEmpty()
+    if (hasNullProperties) println(
+        "========================== Notice ==========================\n" +
+            "You should set $nullProperties in your properties file. \n" +
+            "Otherwise this signingConfig will not be applied.\n" +
+            "The default properties file is ${rootDir}${File.separator}local.properties.\n" +
+            "============================================================"
+    )
+    return hasNullProperties
 }
