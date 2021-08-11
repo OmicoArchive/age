413c17ca9e5e46c88e851eb10ac1c128091da81a
Omico <omico@qq.com>
2021-08-11 02:30:37 -0700

    spotless: Refactoring

--- buildSrc/src/main/kotlin/me/omico/age/dsl/Spotless.kt
+++ buildSrc/src/main/kotlin/me/omico/age/dsl/Spotless.kt
@@ -2,6 +2,9 @@
 
 package me.omico.age.dsl
 
+import com.diffplug.gradle.spotless.FormatExtension
+import com.diffplug.gradle.spotless.KotlinExtension
+import com.diffplug.gradle.spotless.KotlinGradleExtension
 import com.diffplug.gradle.spotless.SpotlessExtension
 import com.diffplug.gradle.spotless.SpotlessPlugin
 import org.gradle.api.Project
@@ -13,31 +16,75 @@ fun Project.configureSpotless(block: SpotlessExtension.() -> Unit) {
     plugins.withId("com.diffplug.spotless") { configure(block) }
 }
 
-fun Project.configureSpotlessWithCommonRules(
-    ktlintVersion: String = "0.41.0",
-) = configureSpotless {
-    kotlin {
-        target("src/**/*.kt")
-        ktlint(ktlintVersion)
-        indentWithSpaces()
-        trimTrailingWhitespace()
-        endWithNewline()
+private const val DEFAULT_KT_LINT_VERSION = "0.41.0"
+
+fun Project.configureSpotlessWithCommonRules() =
+    configureSpotless {
+        androidXml()
+        gradleVersionCatalogs()
+        intelliJIDEARunConfiguration()
+        kotlin()
+        kotlinGradle()
     }
-    kotlinGradle {
-        target("**/*.gradle.kts")
-        ktlint(ktlintVersion)
-        indentWithSpaces()
-        trimTrailingWhitespace()
-        endWithNewline()
-    }
-    format("xml") {
-        target(
-            "src/**/*.xml",
-            ".run/*.xml",
-            ".idea/runConfigurations/*.xml",
-        )
-        indentWithSpaces()
-        trimTrailingWhitespace()
-        endWithNewline()
-    }
+
+fun SpotlessExtension.androidXml(
+    block: FormatExtension.() -> Unit = {},
+) = format("androidXml") {
+    target(
+        "**/AndroidManifest.xml",
+        "src/**/*.xml",
+    )
+    indentWithSpaces()
+    trimTrailingWhitespace()
+    endWithNewline()
+    block()
+}
+
+fun SpotlessExtension.gradleVersionCatalogs(
+    block: FormatExtension.() -> Unit = {},
+) = format("gradleVersionCatalogs") {
+    target(
+        "**/*.versions.toml",
+    )
+    indentWithSpaces()
+    trimTrailingWhitespace()
+    endWithNewline()
+    block()
+}
+
+fun SpotlessExtension.intelliJIDEARunConfiguration(
+    block: FormatExtension.() -> Unit = {},
+) = format("intelliJIDEARunConfiguration") {
+    target(
+        ".run/*.xml",
+        ".idea/runConfigurations/*.xml",
+    )
+    indentWithSpaces()
+    trimTrailingWhitespace()
+    endWithNewline()
+    block()
+}
+
+fun SpotlessExtension.kotlin(
+    ktLintVersion: String = DEFAULT_KT_LINT_VERSION,
+    block: KotlinExtension.() -> Unit = {},
+) = kotlin {
+    target("src/**/*.kt")
+    ktlint(ktLintVersion)
+    indentWithSpaces()
+    trimTrailingWhitespace()
+    endWithNewline()
+    block()
+}
+
+fun SpotlessExtension.kotlinGradle(
+    ktLintVersion: String = DEFAULT_KT_LINT_VERSION,
+    block: KotlinGradleExtension.() -> Unit = {},
+) = kotlinGradle {
+    target("**/*.gradle.kts")
+    ktlint(ktLintVersion)
+    indentWithSpaces()
+    trimTrailingWhitespace()
+    endWithNewline()
+    block()
 }
