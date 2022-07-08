043d82b4b5fa569d4f65fafeb72fabc3b15b9702
Omico <me@omico.icu>
2022-07-08 04:15:07 -0700

    plugin: settings: Allow copy sub-path files

--- age-gradle-plugin/settings/auto-module-creation/build.gradle.kts
+++ age-gradle-plugin/settings/auto-module-creation/build.gradle.kts
@@ -1,3 +1,13 @@
 plugins {
     `kotlin-dsl`
 }
+
+// See: https://github.com/gradle/gradle/issues/21151#issuecomment-1172894670
+afterEvaluate {
+    tasks.compileKotlin {
+        kotlinOptions {
+            apiVersion = "1.5"
+            languageVersion = "1.5"
+        }
+    }
+}
--- age-gradle-plugin/settings/auto-module-creation/src/main/java/me/omico/age/settings/internal/TemplateCreator.kt
+++ age-gradle-plugin/settings/auto-module-creation/src/main/java/me/omico/age/settings/internal/TemplateCreator.kt
@@ -3,38 +3,74 @@ package me.omico.age.settings.internal
 import me.omico.age.settings.AutoModuleCreationConfigs
 import me.omico.age.settings.AutoModuleCreationExtension
 import me.omico.age.settings.group
+import me.omico.age.settings.name
 import me.omico.age.settings.sourceType
 import me.omico.age.settings.template
 import java.io.File
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+import kotlin.io.path.absolutePathString
+import kotlin.io.path.createDirectories
+import kotlin.io.path.exists
+import kotlin.io.path.isDirectory
+import kotlin.io.path.isRegularFile
+import kotlin.io.path.readText
+import kotlin.io.path.writeText
 
 fun AutoModuleCreationExtension.createTemplate(module: String) {
     val settings = AutoModuleCreationConfigs.settings
-    var dir = settings.rootDir
-    currentPath.split(":").forEach { dir = dir.resolve(it) }
-    dir = dir.resolve(module)
-    val srcDir = dir.resolve("src")
-    if (!srcDir.exists()) srcDir.resolve("main").let {
-        it.mkdirs()
+    val moduleDirectory: Path = settings.rootDir.toPath()
+        .resolve(currentPath.removePrefix(":").replace(":", File.separator))
+        .resolve(module)
+    moduleDirectory.createDirectories()
+    val moduleSourceDirectory: Path = moduleDirectory.resolve("src")
+    if (!moduleSourceDirectory.exists()) moduleSourceDirectory.resolve("main").also {
+        it.createDirectories()
         sourceType.ifNotNullOrEmpty { sourceType ->
-            it.resolve(sourceType).mkdirs()
+            it.resolve(sourceType).createDirectories()
             group.ifNotNullOrEmpty { group ->
-                it.resolve(sourceType).resolve(group.replace(".", File.separator)).mkdirs()
+                it.resolve(sourceType).resolve(group.replace(".", File.separator)).createDirectories()
             }
         }
     }
-    dir.copyTemplates(template, variables)
+    copyTemplates(moduleDirectory)
     settings.include("$currentPath:$module")
 }
 
-private fun File.copyTemplates(template: String, variables: Map<String, String>) {
-    val templateFiles = AutoModuleCreationConfigs.templatesFolder.resolve(template).listFiles() ?: return
-    templateFiles.forEach { templateFile ->
-        val targetFile = resolve(templateFile.name)
-        if (targetFile.exists()) return
-        templateFile.readText().let {
-            var content = it
-            variables.forEach { (key, value) -> content = content.replace("@($key)", value) }
-            return@let content
-        }.let { targetFile.writeText(it) }
+private fun AutoModuleCreationExtension.copyTemplates(moduleDirectory: Path) {
+    val templateDictionary = AutoModuleCreationConfigs.templatesFolder.toPath().resolve(template)
+    val templateDictionaryString = templateDictionary.absolutePathString()
+    Files.walk(templateDictionary).forEach {
+        if (it == templateDictionary) return@forEach
+        val target = targetPath(moduleDirectory, templateDictionaryString, it)
+        if (target.exists()) return@forEach
+        when {
+            it.isRegularFile() -> target.writeText(variables.resolveContent(it.readText()))
+            it.isDirectory() -> target.createDirectories()
+        }
     }
+}
+
+private fun AutoModuleCreationExtension.targetPath(
+    moduleDirectory: Path,
+    templateDictionary: String,
+    template: Path,
+): Path =
+    template.absolutePathString()
+        .replace(templateDictionary, moduleDirectory.absolutePathString())
+        .let {
+            when {
+                it.startsWith(moduleDirectory.absolutePathString()) ->
+                    it.replace("@(group)", group.replace(".", File.separator))
+                        .replace("@(name)", name)
+                else -> it
+            }
+        }
+        .let(Paths::get)
+
+fun Map<String, String>.resolveContent(content: String): String {
+    var resolveContent = content
+    forEach { (key, value) -> resolveContent = resolveContent.replace("@($key)", value) }
+    return resolveContent
 }
