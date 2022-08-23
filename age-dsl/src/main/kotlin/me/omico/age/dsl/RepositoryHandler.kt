94318abc1fd33da2cb246b19ca4d646cac2bc134
Omico <me@omico.icu>
2022-08-23 06:11:47 -0700

    Add more extensions for RepositoryHandler

--- age-dsl/src/main/kotlin/me/omico/age/dsl/RepositoryHandler.kt
+++ age-dsl/src/main/kotlin/me/omico/age/dsl/RepositoryHandler.kt
@@ -3,8 +3,51 @@
 package me.omico.age.dsl
 
 import org.gradle.api.artifacts.dsl.RepositoryHandler
+import org.gradle.api.artifacts.repositories.ArtifactRepository
 import org.gradle.api.artifacts.repositories.MavenArtifactRepository
 import org.gradle.kotlin.dsl.maven
 
-fun RepositoryHandler.sonatypeSnapshots(): MavenArtifactRepository =
-    maven(url = "https://oss.sonatype.org/content/repositories/snapshots/")
+fun RepositoryHandler.gradle(): ArtifactRepository =
+    gradlePluginPortal {
+        content {
+            includeGroupByRegex("com.gradle.*")
+            includeGroupByRegex("org.gradle.*")
+            includeGroupByRegex("org.jetbrains.kotlin.*")
+        }
+    }
+
+fun RepositoryHandler.spotless(): ArtifactRepository =
+    gradlePluginPortal {
+        content {
+            includeGroupByRegex("com.diffplug.spotless.*")
+        }
+    }
+
+fun RepositoryHandler.sonatypeSnapshots(
+    groupRegexList: Set<String> = setOf(),
+): MavenArtifactRepository =
+    maven(url = "https://s01.oss.sonatype.org/content/repositories/snapshots") {
+        content {
+            groupRegexList.forEach(::includeGroupByRegex)
+        }
+    }
+
+fun RepositoryHandler.androidxSnapshots(buildID: Int): MavenArtifactRepository =
+    maven(url = "https://androidx.dev/snapshots/builds/$buildID/artifacts/repository")
+
+fun RepositoryHandler.jetbrainsCompose(): MavenArtifactRepository =
+    maven(url = "https://maven.pkg.jetbrains.space/public/p/compose/dev")
+
+fun RepositoryHandler.ageSnapshots(): MavenArtifactRepository =
+    sonatypeSnapshots(
+        groupRegexList = setOf(
+            "me.omico.age.*",
+        ),
+    )
+
+fun RepositoryHandler.gradmSnapshots(): MavenArtifactRepository =
+    sonatypeSnapshots(
+        groupRegexList = setOf(
+            "me.omico.gradm.*",
+        ),
+    )
